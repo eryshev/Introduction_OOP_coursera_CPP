64abb0d809e808defd776893f3390b32aee68ea9
eryshev <a.a.eryshev@gmail.com>
2015-03-03 20:26:20 +0100

    Coursera introduction at OOP(C++)
    week by week Coursera OOP introduction solutions.

---
+++ Week1/Week1/imc.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+using namespace std;
+
+/*****************************************************
+* Compléter le code à partir d'ici
+*****************************************************/
+class Patient {
+private:
+	double weight;
+	double height;
+
+public:
+	void init(double w, double h) {
+		if (w > 0.0 && h > 0.0) {
+			weight = w;
+			height = h;
+		}
+		else {
+			weight = 0.0;
+			height = 0.0;
+		}
+	}
+
+	void afficher() const {
+		cout << "Patient : " << weight << " kg pour " << height << " m" << endl;
+	}
+
+	double poids() const {
+		return weight;
+	}
+
+	double taille() const {
+		return height;
+	}
+
+	double imc() const {
+		if (height == 0.0) return 0.0;
+		else return weight / (height * height);
+	}
+
+};
+
+/*******************************************
+* Ne rien modifier après cette ligne.
+*******************************************/
+
+int main()
+{
+	Patient quidam;
+	double poids, taille;
+	do {
+		cout << "Entrez un poids (kg) et une taille (m) : ";
+		cin >> poids >> taille;
+		quidam.init(poids, taille);
+		quidam.afficher();
+		cout << "IMC : " << quidam.imc() << endl;
+	} while (poids * taille != 0.0);
+	return 0;
+}
---
+++ Week1/Week1/tirelire.cpp
@@ -0,0 +1,101 @@
+﻿#include <iostream>
+using namespace std;
+
+/*****************************************************
+* Compléter le code à partir d'ici
+*****************************************************/
+class Tirelire {
+private:
+	double montant;
+
+public:
+	double getMontant() const {
+		return montant;
+	}
+
+	void afficher() const {
+		double montant = getMontant();
+		if (montant <= 0) cout << "Vous etes sans le sous." << endl;
+		else cout << "Vous avez : " << montant << " euros dans votre tirelire." << endl;
+	}
+
+	void secouer() const {
+		double montant = getMontant();
+		if (montant > 0) cout << "Bing bing" << endl;
+	}
+
+	void remplir(double m) {
+		if ( m >= 0 ) montant = m;
+	}
+
+	void vider() {
+		montant = 0.0;
+	}
+
+	void puiser(double m) {
+		if (m > 0 ) {
+			montant -= m;
+			if (montant < 0) montant = 0.0;
+		}
+	}
+
+	bool montant_suffisant(double budget, double& rest) const {
+		rest = montant - (budget > 0 ? budget : 0);
+		if (rest >= 0) return true;
+		else {
+			rest *= -1;
+			return false;
+		}
+	}
+
+};
+
+/*******************************************
+* Ne rien modifier après cette ligne.
+*******************************************/
+
+int main()
+{
+	Tirelire piggy;
+
+	piggy.vider();
+	piggy.secouer();
+	piggy.afficher();
+
+	piggy.puiser(20.0);
+	piggy.secouer();
+	piggy.afficher();
+
+	piggy.remplir(550.0);
+	piggy.secouer();
+	piggy.afficher();
+
+	piggy.puiser(10.0);
+	piggy.puiser(5.0);
+	piggy.afficher();
+
+	cout << endl;
+
+	// le budget de vos vacances de rève.
+	double budget;
+
+	cout << "Donnez le budget de vos vacances : ";
+	cin >> budget;
+
+	// ce qui resterait dans la tirelire après les
+	// vacances
+	double solde(0.0);
+
+	if (piggy.montant_suffisant(budget, solde)) {
+		cout << "Vous êtes assez riche pour partir en vacances !"
+			<< endl
+			<< "Il vous restera " << solde << " euros"
+			<< " à la rentrée." << endl << endl;
+		piggy.puiser(budget);
+	}
+	else {
+		cout << "Il vous manque " << solde << " euros"
+			<< " pour partir en vacances !" << endl << endl;
+	}
+	return 0;
+}
